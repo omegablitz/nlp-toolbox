decb4e9b41c883089c13289db9f8ae6b2b87d214
Ahsaas Bajaj <ahsaas.bajaj@instabase.com>
2020-07-13 05:11:48 -0400

    add data curation, test data prep, infer model

---
+++ model_framework/framework.py
@@ -0,0 +1,310 @@
+import csv
+import json
+import os
+import pandas as pd
+import re
+import string
+import logging
+import numpy as np
+import math
+
+from transformers import BertModel
+from transformers import BertTokenizer
+import numpy as np
+import torch
+from torch import nn
+import logging
+import itertools
+import difflib
+
+# Point to instabase SDK, use SDK to download
+import sys
+sys.path.append('/Users/ahsaasbajaj/Documents/Code/instabase/sdk/src/py')
+import importlib
+import instabase_sdk
+importlib.reload(instabase_sdk)
+from instabase_sdk import Instabase
+
+PUNC_TABLE = str.maketrans({key: None for key in string.punctuation})
+
+from rule_features import rules
+from preprocessing import preprocessing_rules
+
+from test_classifier import infer_classifier
+
+# Import instabase
+import sys
+sys.path.append(
+    '/Users/ahsaasbajaj/Documents/Code/instabase/distributed-tasks/celery/app-tasks/build/py'
+)
+from instabase.ocr.client.libs.ibocr import ParsedIBOCRBuilder
+
+
+class DatasetWarning():
+    def __init__(self, warning_type, message):
+        self.warning_type = warning_type
+        self.message = message
+    def __repr__(self):
+        return 'Warning[{}, {}]'.format(self.warning_type, self.message)
+
+class Usecase():
+    def __init__(self, usecase):
+        self.usecase = usecase
+    
+    def __repr__(self):
+        return 'Usecase[{}, {}, {}]'.format(self.usecase, self.dataset)
+
+    def setConfig(self, config):
+        self.config = config
+        self.setDataset()
+        self.setLabelsDict()
+
+    def setDataset(self):
+      self.dataset = self.config['dataset']
+
+    def setLabelsDict(self):
+        self.labels_dict = self.config['labels_dict']
+
+
+class StringProcessing():
+    @staticmethod
+    def isSafe(row):
+        # retunrs True if string doesn't contain any numbers
+        if any(char.isdigit() for char in row):
+            return False
+        if len(row) < 5:
+            # string less than 5 characters or single word
+            return False
+        return True
+    
+    @staticmethod
+    def find_remove_opening_token(row, token):
+        # remove quotes from opening
+        indx = row.find(token)
+        if indx == 0:
+            row = row[1:]
+        return row
+
+    @staticmethod
+    def clean_string(strng):
+        names = strng.split(" ")
+        ans = []
+        for p in names:
+            if p != '':
+                ans.append(p)
+                
+        result = " ".join(ans)
+        result = result.lower().strip()
+        return result
+
+class DataCuration():
+
+  def __init__(self, access_token, dataset_paths, dataset_config,
+               goldens_paths, goldens_config):
+    self.ib = Instabase('https://instabase.com', access_token)
+    self.dataset_config = dataset_config
+    self.golden_config = goldens_config
+
+    if type(dataset_paths) != list:
+      dataset_paths = [dataset_paths]
+
+    if type(goldens_paths) != list:
+      goldens_paths = [goldens_paths]
+
+    self.datadir = dataset_paths
+    self._load_ib_dataset(dataset_paths, dataset_config)
+    self._load_goldens(goldens_paths, goldens_config)
+    if dataset_config['convert2txt']:
+        self.processIBOCR2txt(dataset_config)
+
+  def _load_goldens(self, goldens_paths, goldens_config):
+    goldens_type = goldens_config.get('file_type')
+    for goldens_path in goldens_paths:
+      logging.info('Loading goldens from {}'.format(goldens_path))
+      if goldens_type == 'csv':
+        golden_all_df = pd.read_csv(goldens_path)
+        self.golden_all = golden_all_df.set_index(goldens_config['index_field_name'])
+
+        # filter goldens by presence in dataset
+        def ispresent(row):
+            if row in self.dataset:
+                return True
+            return False
+        golden_all_df['in_src'] = golden_all_df[goldens_config['index_field_name']].apply(ispresent)
+        golden_df = golden_all_df[golden_all_df['in_src'] == True]
+        golden_df = golden_df.drop('in_src', axis=1)
+        self.golden = golden_df.set_index(goldens_config['index_field_name'])
+
+        print('Total files Goldens: ', self.golden_all.shape)
+        print('Total files found in the source', self.golden.shape)
+
+  def _load_ib_dataset(self, dataset_paths, dataset_config):
+    self.dataset = {}
+    for dataset_path in dataset_paths:
+      logging.info('Loading dataset from {}'.format(dataset_path))
+      if dataset_config['is_local'] is False:
+          file_result = self.ib.list_dir(dataset_path)
+          files = [node['full_path'] for node in file_result['nodes']]
+          file_objects = []
+          for file in files:
+              with open(file) as f:
+                file_objects.append(self.ib.read_file(f))
+      else:
+          files = os.listdir(dataset_path)
+          file_objects = []
+          for file in files:
+              with open(os.path.join(dataset_path, file)) as f:
+                file_objects.append(f.read())
+
+      for file, file_object in zip(files, file_objects):
+        content = None
+        identifier = file
+        if dataset_config.get('file_type') in ['ibdoc', 'ibocr']:
+          ibocr, err = ParsedIBOCRBuilder.load_from_str(os.path.join(dataset_path, file), file_object)
+          content = ibocr.as_parsed_ibocr()
+        if dataset_config.get('identifier'):
+          identifier = dataset_config.get('identifier')(file)
+        self.dataset.update({identifier: content})
+
+  def processIBOCR2txt(self, dataset_config):
+    #Todo: add reading from ib
+    self.texts = {}
+    for data_dir in self.datadir:
+        files = os.listdir(data_dir)
+        print("Processing {} IBOCR files to txt".format(len(files)))
+
+        for fname in files:
+            fpath = os.path.join(data_dir, fname)
+            f = open(fpath)
+            ext = fpath.split('.')[-1]
+            file = json.load(f)
+            dictionary = file[0]
+            dictionary.keys()
+            texts = dictionary['text']
+            if dataset_config.get('identifier'):
+                identifier = dataset_config.get('identifier')(fname)
+            self.texts.update({identifier: texts})
+
+  def processIBOCR2candidatePhrases(self, dataset_config, processing_config):
+    #Todo: add reading from ib
+
+    self.candidates = {}
+    X_DIST_THRESHOLD = processing_config['X_DIST_THRESHOLD']
+    for data_dir in self.datadir:
+        files = os.listdir(data_dir)
+        print("Generating candidates for {} files".format(len(files)))
+        for fname in files:
+            fpath = os.path.join(data_dir, fname)
+            f = open(fpath)
+            file = json.load(f)
+            dictionary = file[0]
+            dictionary.keys()
+            lines = dictionary['lines']
+
+            words = [[]]
+            for line in lines:
+                for word in line: 
+                    start = word['start_x']
+                    if not words[-1]:
+                        words[-1].append(word)
+                        continue
+                    if start - words[-1][-1]['end_x'] < X_DIST_THRESHOLD:
+                        words[-1].append(word)
+                        continue
+                    words.append([word])
+                words.append([])
+            
+            phrases = []
+            for cluster in words:
+                phrases.append(' '.join([w['word'] for w in cluster]))
+        
+            # remove special characters and other filters
+            candidates = []
+            for phrase in phrases:
+                phrase = StringProcessing.find_remove_opening_token(phrase, '"')
+                if StringProcessing.isSafe(phrase):
+                    candidates.append(phrase)
+
+            if dataset_config.get('identifier'):
+                identifier = dataset_config.get('identifier')(fname)
+            self.candidates.update({identifier: candidates})
+
+  def compare_candidates_and_goldens(self, processing_config, candidates_fields):
+        # goldens may have more keys than dataset
+        filenames = []
+        person_found = 0
+        org_found = 0
+        person_found_files = []
+        org_found_files = []
+
+        for key in self.candidates:
+            phrases = self.candidates[key]     
+            cds = set([StringProcessing.clean_string(elt) for elt in phrases])
+            if key not in self.golden.index:
+                continue
+
+            this_person = StringProcessing.clean_string(self.golden.loc[key, candidates_fields['person']])
+            this_org = StringProcessing.clean_string(self.golden.loc[key, candidates_fields['org']])
+            
+            if this_person in cds:
+                person_found += 1
+                person_found_files.append(key)
+            if this_org in cds:
+                org_found += 1
+                org_found_files.append(key)
+            
+        #     if this_person not in cds:
+        #         print(key)
+        #         print(this_person, difflib.get_close_matches(this_person, cds), '\n')
+            # if this_org not in cds:
+            #     print(key)
+            #     print(this_org, difflib.get_close_matches(this_org, cds), '\n')
+            filenames.append(key)
+
+
+        total_files = len(filenames)
+        print("For X_DIST_THRESHOLD configuraion: {0}".format(processing_config['X_DIST_THRESHOLD']))
+        print("total files: {0}\nperson names found in candidates: {1}\norg names found in candidates: {2}\n".format(total_files, len(person_found_files), len(org_found_files)))
+    
+
+class FeatureEngineering():
+    def __init__(self, usecase, data_curation, candidates_fields):
+        self.labels_dict = usecase.labels_dict
+        self.data = data_curation
+        self.candidates_fields = candidates_fields
+
+    def generate_test_samples(self):
+        return
+
+    def generate_test_samples_from_goldens(self):
+        labels = []
+        contexts = []
+
+        persons = self.data.golden[self.candidates_fields['person']].tolist()
+        person_label = [self.labels_dict['person']] * len(persons)
+
+        contexts.extend(persons)
+        labels.extend(person_label)
+
+        org = self.data.golden[self.candidates_fields['org']].tolist()
+        org_label = [self.labels_dict['org']] * len(org)
+
+        contexts.extend(org)
+        labels.extend(org_label)
+
+        test_samples = pd.DataFrame(list(zip(contexts, labels)), columns=['context', 'label'])
+        test_samples = test_samples.sample(frac=1)
+        test_samples = test_samples.dropna()
+        return test_samples
+
+
+class ModelEvaluator():
+    def setModelConfig(self, model_config):
+        self.model_config = model_config
+
+    def runEvaluation(self, testdata):
+        model_file_or_path = self.model_config['model_file_or_path']
+        num_labels = self.model_config['num_labels']
+        gpu = self.model_config['gpu']
+
+        return infer_classifier(model_file_or_path, testdata, num_labels, gpu)
---
+++ model_framework/test_classifier.py
@@ -0,0 +1,115 @@
+import argparse
+import data_utils
+import torch
+from torch.utils.data import TensorDataset, DataLoader, SequentialSampler
+from sklearn.metrics import f1_score, precision_score, recall_score
+import pandas as pd
+import numpy as np
+import os, sys
+from data_utils import readData, flat_accuracy, save_plots_models
+from transformers import BertForSequenceClassification, BertTokenizer
+
+sys.path.insert(0, os.path.abspath('..'))
+os.environ["PYTHONIOENCODING"] = "utf-8"
+
+def infer_classifier(modelPath, testdf, num_labels, gpu):
+    model_name_or_path = "bert-large-cased"
+    device = torch.device("cuda" if (gpu and torch.cuda.is_available()) else "cpu")
+    print('Device: ', device)
+
+    model_state_dict = torch.load(modelPath, map_location=device)
+    model = BertForSequenceClassification.from_pretrained(model_name_or_path, state_dict=model_state_dict, num_labels=num_labels)
+    print("Fine-tuned model loaded with labels = ", model.num_labels)
+
+    model = model.to(device)
+    model.eval()
+
+    tokenizer = BertTokenizer.from_pretrained(model_name_or_path, do_lower_case=True)
+
+    # testing
+    input_ids, labels, attention_masks = readData(tokenizer, testdf)
+    prediction_inputs = torch.tensor(input_ids)
+    prediction_masks = torch.tensor(attention_masks)
+    prediction_labels = torch.tensor(labels)
+
+    batch_size = 32
+    prediction_data = TensorDataset(prediction_inputs, prediction_masks, prediction_labels)
+    prediction_sampler = SequentialSampler(prediction_data)
+    prediction_dataloader = DataLoader(prediction_data, sampler=prediction_sampler, batch_size=batch_size)
+
+    model.eval()
+
+    # Tracking variables
+    predictions, true_labels = [], []
+    nb_eval_steps = 0
+    eval_accuracy = 0
+    csv_output = []
+
+    # Predict
+    for batch in prediction_dataloader:
+        # Add batch to GPU
+        batch = tuple(t.to(device) for t in batch)
+        # Unpack the inputs from our dataloader
+        b_input_ids, b_input_mask, b_labels = batch
+        # Telling the model not to compute or store gradients, saving memory and speeding up prediction
+        with torch.no_grad():
+            # Forward pass, calculate logit predictions
+            outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+
+        logits = outputs[0]
+        # shape (batch_size, config.num_labels)
+        # Move logits and labels to CPU
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+        # Store predictions and true labels
+        predictions.append(logits)
+        true_labels.append(label_ids)
+        tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+        eval_accuracy += tmp_eval_accuracy
+        nb_eval_steps += 1
+        pred_flat = np.argmax(logits, axis=1).flatten()
+        labels_flat = label_ids.flatten()
+        count = 0
+        for i in range(pred_flat.shape[0]):
+            # iterate over the batch
+            csv_output.append((b_input_ids[i], pred_flat[i], labels_flat[i]))
+
+    validLabels = [x for x in labels if str(x) != 'nan']
+
+    if len(validLabels) != 0:
+        # otherwise it is inference mode with all candidate phrases (with label = NA)
+        print('Test Accuracy Accuracy: {0:0.4f}'.format((float(eval_accuracy) / float(nb_eval_steps))))
+
+        flat_predictions = [item for sublist in predictions for item in sublist]
+        flat_predictions = np.argmax(flat_predictions, axis=1).flatten()
+        flat_true_labels = [item for sublist in true_labels for item in sublist]
+
+        labels = [x for x in range(num_labels - 1)]    # don't include 'None' class
+
+        micro_precision = precision_score(flat_true_labels, flat_predictions, labels=labels, average="micro")
+        micro_recall = recall_score(flat_true_labels, flat_predictions,  labels=labels, average="micro")
+        micro_f1 = f1_score(flat_true_labels, flat_predictions, labels=labels, average="micro")
+        print('Micro Test R: {0:0.4f}, P: {1:0.4f}, F1: {2:0.4f}'.format(micro_recall, micro_precision, micro_f1))
+
+        macro_precision = precision_score(flat_true_labels, flat_predictions, labels=labels, average="macro")
+        macro_recall = recall_score(flat_true_labels, flat_predictions, labels=labels, average="macro")
+        macro_f1 = f1_score(flat_true_labels, flat_predictions, labels=labels, average="macro")
+        print('Macro Test R: {0:0.4f}, P: {1:0.4f}, F1: {2:0.4f}'.format(macro_recall, macro_precision, macro_f1))
+
+    sentences = testdf['context'].tolist()  # un-tokenized sentences
+
+    assert (len(sentences) == len(csv_output))
+
+    headings = ['context', 'predicted', 'label']
+    df = pd.DataFrame(columns=headings)
+    index = 0
+    for ids, pred, label in csv_output:
+        ids = np.trim_zeros(ids.cpu().numpy())
+        sentence = tokenizer.convert_ids_to_tokens(ids)[1:-1]
+        # data = [{'a': 1, 'b': 2}, {'a': 5, 'b': 10, 'c': 20}]
+        data = [{'context': sentences[index], 'predicted': str(pred), 'label': str(label)}]
+        df = df.append(pd.DataFrame(data, columns=headings))
+        index += 1
+
+
+    return df
